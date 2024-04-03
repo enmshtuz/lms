199544cf4506b06293fb6c65976cbf1808a45856
Elen Matevosyan <elen.matevosyan90@gmail.com>
2024-04-03 14:17:27 +0400

    Updating docker configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,16 @@
 echo "Applying database migrations..."
 python manage.py migrate
 
-# Create admin user if not exists
-echo "Creating admin user..."
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
 
+# Check if admin user exists
+if ! python manage.py shell -c "from django.contrib.auth.models import User; exit(0) if User.objects.filter(username='admin').exists() else exit(1)"; then
+    # Create admin user
+    echo "Creating admin user..."
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+fi
 
 # Start the Django server
 exec "$@"
