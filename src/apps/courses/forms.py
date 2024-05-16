4cccd738a1aa96bf1a76ff3054108c3e0e8b92dd
Elen Matevosyan <elen.matevosyan90@gmail.com>
2024-05-16 23:12:16 +0400

    Learning Progress

--- src/apps/courses/forms.py
+++ src/apps/courses/forms.py
@@ -9,31 +9,15 @@ class CourseForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['file'].widget.attrs.update({'class': 'form-control-file'})
-        self.fields['video'].widget.attrs.update({'class': 'form-control-file'})
-
-    def clean(self):
-        cleaned_data = super().clean()
-        file = cleaned_data.get('file')
-        video = cleaned_data.get('video')
-        url = cleaned_data.get('url')
-
-        # Check that at least one of the fields (file, video, url) is filled out
-        if not file and not video and not url:
-            raise forms.ValidationError('Please upload a file, video, or enter a URL.')
-
-        # Check that only one of the fields (file, video, url) is filled out
-        fields_filled = sum(bool(field) for field in [file, video, url])
-        if fields_filled > 1:
-            raise forms.ValidationError('Please choose only one of file, video, or URL.')
-
-        return cleaned_data
+        self.fields['file'].widget.attrs.update({'class': 'form-control-file', 'multiple': 'True'})
+        self.fields['video'].widget.attrs.update({'class': 'form-control-file', 'multiple': 'True'})
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['level'].widget.attrs.update({'class': 'form-control'})
+        self.fields['url'].widget.attrs.update({'class': 'form-control'})
 
 
-# class CourseEnrollmentForm(forms.ModelForm):
-#     class Meta:
-#         model = CourseEnrollment
-#         fields = ['course', 'user', 'enrollment_type']
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
+
+
+
+
--- src/main/urls.py
+++ src/main/urls.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import src.apps.courses.views as courses_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('ad/', admin.site.urls),
+    path('admin/courses/', courses_view.course_list, name='course_list'),
     path('', include('admin_soft.urls')),
     path('quiz/', include('src.apps.quiz.urls')),
     path('account/', include('src.apps.userAuth.urls')),
-    path('courses/', include('src.apps.courses.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('course/', include('src.apps.courses.urls')),
+    path('courses/', courses_view.published_course_list, name='published_course_list')
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
