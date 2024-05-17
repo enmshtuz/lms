8c06c3b5d5ccac0398a7d3b846ed5dcd7332e02c
astghikboyajyan <165201829+astghikboyajyan@users.noreply.github.com>
2024-05-17 12:29:33 +0400

    Sidebar and role management (#15)

--- src/main/urls.py
+++ src/main/urls.py
@@ -5,15 +5,14 @@ from django.conf.urls.static import static
 import src.apps.courses.views as courses_view
 
 urlpatterns = [
-    path('ad/', admin.site.urls),
-    path('admin/courses/', courses_view.course_list, name='course_list'),
-    path('', include('admin_soft.urls')),
-    path('quiz/', include('src.apps.quiz.urls')),
-    path('account/', include('src.apps.userAuth.urls')),
-    path('course/', include('src.apps.courses.urls')),
-    path('courses/', courses_view.published_course_list, name='published_course_list')
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+                  path('ad/', admin.site.urls),
+                  path('admin/courses/', courses_view.course_list, name='course_list'),
+                  path('', include('admin_soft.urls')),
+                  path('quiz/', include('src.apps.quiz.urls')),
+                  path('account/', include('src.apps.userAuth.urls')),
+                  path('course/', include('src.apps.courses.urls')),
+                  path('courses/', courses_view.published_course_list, name='published_course_list')
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
