914329ef8d438670581e518646675ccbbac7eb80
unknown <you@example.com>
2024-05-17 12:33:25 +0400

    Feedback modification

--- src/apps/feedback/models.py
+++ src/apps/feedback/models.py
@@ -3,14 +3,13 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Course(models.Model):
-    title = models.CharField(max_length=100)
-    # Other course fields
-
+# class Course(models.Model):
+#     title = models.CharField(max_length=100)
+#
 
 class Feedback(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    # course = models.ForeignKey(Course, on_delete=models.CASCADE)
     comment = models.TextField(blank=True)
     rating = models.IntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
 
--- src/apps/feedback/views.py
+++ src/apps/feedback/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import FeedbackForm
 from .models import Course
@@ -18,3 +18,21 @@ def submit_feedback(request, course_id):
     else:
         form = FeedbackForm()
     return render(request, 'submit_feedback.html', {'form': form, 'course': course})
+
+# @login_required
+# def submit_feedback(request, course_id):
+#     # Use get_object_or_404 to handle DoesNotExist exception
+#     course = get_object_or_404(Course, pk=course_id)
+#
+#     if request.method == 'POST':
+#         form = FeedbackForm(request.POST)
+#         if form.is_valid():
+#             feedback = form.save(commit=False)
+#             feedback.user = request.user
+#             feedback.course = course
+#             feedback.save()
+#             return redirect('course_detail', course_id=course_id)
+#     else:
+#         form = FeedbackForm()
+#
+#     return render(request, 'submit_feedback.html', {'form': form, 'course': course})
